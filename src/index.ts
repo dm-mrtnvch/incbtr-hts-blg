333ab63c0f7036ef5f0a8a94b57e2b77089b88d3
dm-mrtnvch <dmvshn@gmail.com>
2023-08-27 21:49:31 +0400

    config: vercel.json

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import {BlogsRouter} from "./routes/blogs";
 
 export const app = express()
 app.use(express.json())
-const PORT = 3020
+const port = process.env.PORT || 3020
 
 app.use('/blogs', BlogsRouter)
 
-app.listen(PORT, () => {
-  console.log(`app running on ${PORT} port`)
+app.listen(port, () => {
+  console.log(`app running on ${port} port`)
 })
 
