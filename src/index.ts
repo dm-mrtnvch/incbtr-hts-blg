37009574cd336a7af9a45c530a115a643d3b0412
dm-mrtnvch <dmvshn@gmail.com>
2023-08-28 00:12:03 +0400

    fix: unreachable timeout

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ const app = express()
 app.use(express.json())
 const port = process.env.PORT || 3020
 
-app.use('/blogs', BlogsRouter)
-app.use('/posts', postsRouter)
-
 app.delete('/testing/all-data', (req: Request, res: Response) => {
   blogsDb.length = 0
   postsDb.length = 0
   res.sendStatus(204)
 })
+
+app.use('/blogs', BlogsRouter)
+app.use('/posts', postsRouter)
 
 app.listen(port, () => {
   console.log(`app running on ${port} port`)
