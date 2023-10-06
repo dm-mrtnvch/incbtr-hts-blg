b3d21a23170bdedc3170a09ca195f3b9390c7449
dm-mrtnvch <dmvshn@gmail.com>
2023-10-07 00:43:50 +0200

    ref: post routes to PostsController class

--- src/routes/posts.router.ts
+++ src/routes/posts.router.ts
@@ -1,7 +1,7 @@
 import {Response, Router} from "express";
 import {body, param, query} from "express-validator";
 import {SortDirection, UUID} from "mongodb";
-import {errorsValidation, sortDirectionValueOrUndefined, toNumberOrUndefined} from "../helpers/utils";
+import {sortDirectionValueOrUndefined, toNumberOrUndefined} from "../helpers/utils";
 import {
   RequestWithBody,
   RequestWithParams,
@@ -21,19 +21,15 @@ import {usersQueryRepository} from "../repositories/users/query";
 import {commentsService} from "../services/comments.service";
 import {postsService} from "../services/posts.service";
 
-
 export const postsRouter = Router()
 
-postsRouter.get('/',
-  query('pageNumber').customSanitizer(toNumberOrUndefined),
-  query('pageSize').customSanitizer(toNumberOrUndefined),
-  query('sortDirection').customSanitizer(sortDirectionValueOrUndefined),
-  async (req: RequestWithQuery<{
+class PostsController {
+  async getPosts(req: RequestWithQuery<{
     pageNumber?: number,
     pageSize?: number,
     sortBy?: string,
     sortDirection?: SortDirection
-  }>, res: Response) => {
+  }>, res: Response) {
     const {pageNumber, pageSize, sortBy, sortDirection} = req.query
 
     const posts = await postsService.getAllPosts(
@@ -42,10 +38,9 @@ postsRouter.get('/',
       sortBy,
       sortDirection)
     res.send(posts)
-  })
+  }
 
-postsRouter.get('/:id',
-  async (req: RequestWithParams<{ id: string }>, res: Response) => {
+  async getPost(req: RequestWithParams<{ id: string }>, res: Response) {
     const {id} = req.params
     const post = await postsQueryRepository.getPostById(id)
 
@@ -54,79 +49,38 @@ postsRouter.get('/:id',
     } else {
       res.sendStatus(404)
     }
-  })
+  }
 
-postsRouter.post('/',
-  BasicAuthMiddleware,
-  body('title').trim().notEmpty().isLength({max: 30}),
-  body('shortDescription').trim().notEmpty().isLength({max: 100}),
-  body('content').trim().notEmpty().isLength({max: 1000}),
-  body('blogId').trim().notEmpty().custom(async (blogId) => {
-    const blog = await blogsQueryRepository.getBlogById(blogId);
-    if (!blog) {
-      throw new Error('Blog not found');
-    }
-  }).withMessage('Specified blog does not exist.'),
-  param('blogId').customSanitizer(value => new UUID(value)),
-  RequestErrorsValidationMiddleware,
-
-  async (req: RequestWithBody<{
+  async createPost(req: RequestWithBody<{
     title: string,
     shortDescription: string,
     content: string,
     blogId: string
-  }>, res: Response) => {
+  }>, res: Response) {
     const {title, shortDescription, content, blogId} = req.body
-
-    // const errors = errorsValidation(req, res)
-    // if (errors?.errorsMessages?.length) {
-    //   res.status(400).send(errors)
-    //   return
-    // }
-
     const newPost = await postsService.createPost(title, shortDescription, content, blogId)
+
     if (newPost) {
       res.status(201).send(newPost)
     } else {
       res.sendStatus(400)
     }
-  })
+  }
 
-postsRouter.put('/:id',
-  BasicAuthMiddleware,
-  body('title').trim().notEmpty().isLength({max: 30}),
-  body('shortDescription').trim().notEmpty().isLength({max: 100}),
-  body('content').trim().notEmpty().isLength({max: 1000}),
-  body('blogId').trim().notEmpty().custom(async (blogId, {req}) => {
-    const blog = await blogsQueryRepository.getBlogById(blogId);
-    if (!blog) {
-      throw new Error('Blog not found');
-    }
-  }).withMessage('Specified blog does not exist.'),
-  RequestErrorsValidationMiddleware,
-
-
-  async (req: RequestWithParamsAndBody<{ id: string }, {
+  async updatePost(req: RequestWithParamsAndBody<{ id: string }, {
     title: string,
     shortDescription: string,
     content: string,
     blogId: string
-  }>, res: Response) => {
+  }>, res: Response) {
     const {id} = req.params
     const {title, blogId, content, shortDescription} = req.body
-
     const post = await postsQueryRepository.getPostById(id)
 
     if (!post) {
       res.sendStatus(404)
       return
     }
-
-    // const errors = errorsValidation(req, res)
-    // if (errors?.errorsMessages?.length) {
-    //   res.status(400).send(errors)
-    //   return
-    // }
 
     const isUpdated = await postsService.updatePostById(id, title, shortDescription, content, blogId)
 
@@ -135,12 +89,9 @@ postsRouter.put('/:id',
     } else {
       res.sendStatus(404)
     }
-  })
+  }
 
-postsRouter.delete('/:id',
-  BasicAuthMiddleware,
-  async (req: RequestWithParams<{ id: string }>, res: Response) => {
-    /// it's string not number
+  async deletePost(req: RequestWithParams<{ id: string }>, res: Response) {
     const {id} = req.params
     const isPostDeleted = await postsService.deletePostById(id)
 
@@ -149,22 +100,11 @@ postsRouter.delete('/:id',
     } else {
       res.sendStatus(404)
     }
-  })
+  }
 
-postsRouter.post('/:postId/comments',
-  AccessTokenAuthMiddleware,
-  body('content').notEmpty().trim().isLength({min: 20, max: 300}),
-  async (req: RequestWithParamsAndBody<{ postId: string }, { content: string }>, res: Response) => {
-
+  async createComment(req: RequestWithParamsAndBody<{ postId: string }, { content: string }>, res: Response) {
     const {postId} = req.params
     const {content} = req.body
-
-    const errors = errorsValidation(req, res)
-    if (errors?.errorsMessages?.length) {
-      res.status(400).send(errors)
-      return
-    }
-
     const post = await postsQueryRepository.getPostById(postId)
 
     if (!post) {
@@ -184,29 +124,21 @@ postsRouter.post('/:postId/comments',
     } else {
       res.sendStatus(401)
     }
-  })
+  }
 
-/// is id ok here or better postId
-postsRouter.get('/:postId/comments',
-  query('pageNumber').customSanitizer(toNumberOrUndefined),
-  query('pageSize').customSanitizer(toNumberOrUndefined),
-  query('sortDirection').customSanitizer(sortDirectionValueOrUndefined),
-  LightAccessTokenAuthMiddleware,
-  async (req: RequestWithParamsAndQuery<
+  async getComments(req: RequestWithParamsAndQuery<
     { postId: string },
     {
       pageNumber?: number,
       pageSize?: number,
       sortBy?: string,
       sortDirection?: SortDirection
-    }>, res: Response) => {
-
-  const {postId} = req.params
-  const {pageNumber, pageSize, sortBy, sortDirection} = req.query
-
+    }>, res: Response) {
+    const {postId} = req.params
+    const {pageNumber, pageSize, sortBy, sortDirection} = req.query
     const post = await postsQueryRepository.getPostById(postId)
 
-    if(!post){
+    if (!post) {
       res.sendStatus(404)
       return
     }
@@ -214,4 +146,69 @@ postsRouter.get('/:postId/comments',
     const comments = await commentsService.getCommentsByPostId(postId, pageNumber, pageSize, sortBy, sortDirection, req.userId)
 
     res.send(comments)
-  })
+  }
+}
+
+const postsController = new PostsController()
+
+postsRouter.get('/',
+  query('pageNumber').customSanitizer(toNumberOrUndefined),
+  query('pageSize').customSanitizer(toNumberOrUndefined),
+  query('sortDirection').customSanitizer(sortDirectionValueOrUndefined),
+  postsController.getPosts
+)
+
+postsRouter.get('/:id',
+  postsController.getPost
+)
+
+postsRouter.post('/',
+  BasicAuthMiddleware,
+  body('title').trim().notEmpty().isLength({max: 30}),
+  body('shortDescription').trim().notEmpty().isLength({max: 100}),
+  body('content').trim().notEmpty().isLength({max: 1000}),
+  body('blogId').trim().notEmpty().custom(async (blogId) => {
+    const blog = await blogsQueryRepository.getBlogById(blogId);
+    if (!blog) {
+      throw new Error('Blog not found');
+    }
+  }).withMessage('Specified blog does not exist.'),
+  param('blogId').customSanitizer(value => new UUID(value)),
+  RequestErrorsValidationMiddleware,
+  postsController.createPost
+)
+
+postsRouter.put('/:id',
+  BasicAuthMiddleware,
+  body('title').trim().notEmpty().isLength({max: 30}),
+  body('shortDescription').trim().notEmpty().isLength({max: 100}),
+  body('content').trim().notEmpty().isLength({max: 1000}),
+  body('blogId').trim().notEmpty().custom(async (blogId, {req}) => {
+    const blog = await blogsQueryRepository.getBlogById(blogId);
+    if (!blog) {
+      throw new Error('Blog not found');
+    }
+  }).withMessage('Specified blog does not exist.'),
+  RequestErrorsValidationMiddleware,
+  postsController.updatePost
+)
+
+postsRouter.delete('/:id',
+  BasicAuthMiddleware,
+  postsController.deletePost
+)
+
+postsRouter.post('/:postId/comments',
+  AccessTokenAuthMiddleware,
+  RequestErrorsValidationMiddleware,
+  body('content').notEmpty().trim().isLength({min: 20, max: 300}),
+  postsController.createComment
+)
+
+postsRouter.get('/:postId/comments',
+  query('pageNumber').customSanitizer(toNumberOrUndefined),
+  query('pageSize').customSanitizer(toNumberOrUndefined),
+  query('sortDirection').customSanitizer(sortDirectionValueOrUndefined),
+  LightAccessTokenAuthMiddleware,
+  postsController.getComments
+)
